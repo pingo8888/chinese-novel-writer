c0a7af5c0d00ad145d9688e0cbcc285c69c0397c
pingo8888 <pingo8888@outlook.com>
2026-02-16 07:56:26 +0800

    功能测试

---
+++ src/constants.ts
@@ -0,0 +1,23 @@
+// 作家助手视图类型
+export const VIEWS = {
+  WRITER_HELPER: "writer-helper",
+} as const;
+
+// 容器常量
+export const CONTAINERS = {
+} as const;
+
+// 文本常量
+export const TEXTS = {
+  WRITER_HELPER: "作家助手",
+  OPEN_WRITER_HELPER: "打开作家助手",
+} as const;
+
+// 图标常量
+export const ICONS = {
+  NOTEBOOK: "notebook-tabs",
+  FOLDER_CLOSED: "folder-closed",
+  FOLDER_OPEN: "folder-open",
+  EXPAND: "list-chevrons-up-down",
+  COLLAPSE: "list-chevrons-down-up",
+} as const;
--- src/main.ts
+++ src/main.ts
@@ -1,99 +1,44 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
+import { Plugin } from "obsidian";
+import { VIEWS, TEXTS, ICONS } from "./constants";
+import ChineseWriterView from "./chinese-writer-view";
 
-// Remember to rename these classes and interfaces!
+export default class ChineseWriterPlugin extends Plugin {
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  async onload() {
+    // 注册作家助手视图
+    this.registerView(VIEWS.WRITER_HELPER, (leaf) => new ChineseWriterView(leaf));
 
-	async onload() {
-		await this.loadSettings();
+    // 激活右边栏作家助手视图
+    this.addCommand({
+      id: VIEWS.WRITER_HELPER,
+      name: TEXTS.OPEN_WRITER_HELPER,
+      callback: () => this.activateRightView()
+    });
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
+    // 添加左边ribbon按钮
+    this.addRibbonIcon(ICONS.NOTEBOOK, TEXTS.WRITER_HELPER, () => this.activateRightView());
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
+  }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+  // 插件卸载时，关闭所有作家助手视图
+  async onunload(): Promise<void> {
+    const existingleaves = this.app.workspace.getLeavesOfType(VIEWS.WRITER_HELPER);
+    existingleaves.forEach(leaf => leaf.detach());
+  }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
-	}
-
-	onunload() {
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+  // 激活右边栏作家助手视图
+  async activateRightView() {
+    const existingleaves = this.app.workspace.getLeavesOfType(VIEWS.WRITER_HELPER);
+    if (existingleaves.length > 0) {
+      existingleaves.slice(1).forEach(leaf => leaf.detach());
+      this.app.workspace.revealLeaf(existingleaves[0]!);
+      this.app.workspace.setActiveLeaf(existingleaves[0]!);
+      return;
+    }
+    const leaf = await this.app.workspace.getRightLeaf(false);
+    if (!leaf) return;
+    await leaf.setViewState({ type: VIEWS.WRITER_HELPER, active: true });
+    this.app.workspace.revealLeaf(leaf);
+    this.app.workspace.setActiveLeaf(leaf);
+  }
 }
